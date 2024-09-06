60fc623a49c35719359ec7f5f45a44ee1825da77
IsabelaDsilva <00001118613727sp@al.educacao.sp.gov.br>
2024-09-06 09:15:00 -0300

    adicionando arquivos js css

---
+++ sketch.js
@@ -0,0 +1,53 @@
+let campoIdade;
+let campoFantasia;
+let campoAventura;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}
+
+function draw() {
+  background("white");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "Harry Potter e a pedra filosofal";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Meu malvado favorito 4";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "Coraline e o Mundo Secreto";
+        } else {
+          return "A bela e a Fera";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "Branca de Neve e o Caçador";
+    } else {
+      return "Shrek";
+    }
+  }
+}
